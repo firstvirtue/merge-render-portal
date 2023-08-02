a4f4f8fc8b5481f6a2bed99a3d7394774597e373
Jake Lee <firstvirtue@gmail.com>
2023-08-03 00:29:04 +0900

    Initialization

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import * as THREE from 'three'
+import { useEffect, useRef, useState } from 'react'
+import { Canvas, extend, useFrame, useThree, createPortal } from '@react-three/fiber'
+import { useCursor, MeshPortalMaterial, CameraControls, Gltf, Text } from '@react-three/drei'
+import { useFBO, useGLTF, useScroll, Image, Scroll, Preload, ScrollControls, MeshTransmissionMaterial } from '@react-three/drei'
+import { useRoute, useLocation } from 'wouter'
+import { easing, geometry } from 'maath'
+import { suspend } from 'suspend-react'
+
+extend(geometry)
+const regular = import('@pmndrs/assets/fonts/inter_regular.woff')
+const medium = import('@pmndrs/assets/fonts/inter_medium.woff')
+
+export const App = () => (
+  <Canvas camera={{ fov: 75, position: [0, 0, 20] }} eventSource={document.getElementById('root')} eventPrefix="client">
+    <color attach="background" args={['#f0f0f0']} />
+    <Lens>
+      <Frame id="01" name={`pick\nles`} author="Omar Faruq Tawsif" bg="#e4cdac" position={[-1.15, 0, 0]} rotation={[0, 0.5, 0]}>
+        <Gltf src="pickles_3d_version_of_hyuna_lees_illustration-transformed.glb" scale={8} position={[0, -0.7, -2]} />
+      </Frame>
+      <Frame id="02" name="tea" author="Omar Faruq Tawsif">
+        <Gltf src="fiesta_tea-transformed.glb" position={[0, -2, -3]} />
+      </Frame>
+      <Frame id="03" name="still" author="Omar Faruq Tawsif" bg="#d1d1ca" position={[1.15, 0, 0]} rotation={[0, -0.5, 0]}>
+        <Gltf src="still_life_based_on_heathers_artwork-transformed.glb" scale={2} position={[0, -0.8, -4]} />
+      </Frame>
+      <Rig />
+    </Lens>
+  </Canvas>
+)
+
+function Frame({ id, name, author, bg, width = 1, height = 1.61803398875, children, ...props }) {
+  const portal = useRef()
+  const [, setLocation] = useLocation()
+  const [, params] = useRoute('/item/:id')
+  const [hovered, hover] = useState(false)
+  useCursor(hovered)
+  useFrame((state, dt) => easing.damp(portal.current, 'blend', params?.id === id ? 1 : 0, 0.2, dt))
+  return (
+    <group {...props}>
+      <Text font={suspend(medium).default} fontSize={0.3} anchorY="top" anchorX="left" lineHeight={0.8} position={[-0.375, 0.715, 0.01]} material-toneMapped={false}>
+        {name}
+      </Text>
+      <Text font={suspend(regular).default} fontSize={0.1} anchorX="right" position={[0.4, -0.659, 0.01]} material-toneMapped={false}>
+        /{id}
+      </Text>
+      <Text font={suspend(regular).default} fontSize={0.04} anchorX="right" position={[0.0, -0.677, 0.01]} material-toneMapped={false}>
+        {author}
+      </Text>
+      <mesh name={id} onDoubleClick={(e) => (e.stopPropagation(), setLocation('/item/' + e.object.name))} onPointerOver={(e) => hover(true)} onPointerOut={() => hover(false)}>
+        <roundedPlaneGeometry args={[width, height, 0.1]} />
+        <MeshPortalMaterial ref={portal} events={params?.id === id} side={THREE.DoubleSide}>
+          <color attach="background" args={[bg]} />
+          {children}
+        </MeshPortalMaterial>
+      </mesh>
+    </group>
+  )
+}
+
+function Rig({ position = new THREE.Vector3(0, 0, 2), focus = new THREE.Vector3(0, 0, 0) }) {
+  const { controls, scene } = useThree()
+  const [, params] = useRoute('/item/:id')
+  useEffect(() => {
+    const active = scene.getObjectByName(params?.id)
+    if (active) {
+      active.parent.localToWorld(position.set(0, 0.5, 0.25))
+      active.parent.localToWorld(focus.set(0, 0, -2))
+    }
+    controls?.setLookAt(...position.toArray(), ...focus.toArray(), true)
+  })
+  return <CameraControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 2} />
+}
+
+function Lens({ children, damping = 0.15, ...props }) {
+  const ref = useRef()
+  // const { nodes } = useGLTF('/lens-transformed.glb')
+  const buffer = useFBO()
+  const viewport = useThree((state) => state.viewport)
+  const [scene] = useState(() => new THREE.Scene())
+  useFrame((state, delta) => {
+    // Tie lens to the pointer
+    // getCurrentViewport gives us the width & height that would fill the screen in threejs units
+    // By giving it a target coordinate we can offset these bounds, for instance width/height for a plane that
+    // sits 15 units from 0/0/0 towards the camera (which is where the lens is)
+    const viewport = state.viewport.getCurrentViewport(state.camera, [0, 0, 20])
+    easing.damp3(
+      ref.current.position,
+      [(state.pointer.x * viewport.width) / 2, (state.pointer.y * viewport.height) / 2, 15],
+      damping,
+      delta
+    )
+    // This is entirely optional but spares us one extra render of the scene
+    // The createPortal below will mount the children of <Lens> into the new THREE.Scene above
+    // The following code will render that scene into a buffer, whose texture will then be fed into
+    // a plane spanning the full screen and the lens transmission material
+    state.gl.setRenderTarget(buffer)
+    state.gl.setClearColor('#d8d7d7')
+    state.gl.render(scene, state.camera)
+    state.gl.setRenderTarget(null)
+  })
+  return (
+    <>
+      {createPortal(children, scene)}
+      <mesh scale={[viewport.width, viewport.height, 1]}>
+        <planeGeometry />
+        <meshBasicMaterial map={buffer.texture} />
+      </mesh>
+      <mesh scale={0.25} ref={ref} rotation-x={Math.PI / 2} {...props}>
+        <sphereBufferGeometry args={[1, 30, 30]} />
+        <MeshTransmissionMaterial buffer={buffer.texture} ior={1.2} thickness={1.5} anisotropy={0.1} chromaticAberration={0.04} />
+      </mesh>
+    </>
+  )
+}
