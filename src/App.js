67f64966a9bf767385460ca035df0270da688e1a
Jake Lee <firstvirtue@gmail.com>
2023-12-22 23:33:45 +0900

    Update modules

--- src/App.js
+++ src/App.js
@@ -54,13 +54,13 @@ function Frames() {
   return (
     <group ref={group}>
         <Frame id="01" name={`pick\nles`} author="Omar Faruq Tawsif" bg="#e4cdac" position={[-1.15, 0, 0]} rotation={[0, 0.5, 0]}>
-          {/* <Gltf src="pickles_3d_version_of_hyuna_lees_illustration-transformed.glb" scale={8} position={[0, -0.7, -2]} /> */}
+          <Gltf src="pickles_3d_version_of_hyuna_lees_illustration-transformed.glb" scale={8} position={[0, -0.7, -2]} />
         </Frame>
         <Frame id="02" name="tea" author="Omar Faruq Tawsif">
-          {/* <Gltf src="fiesta_tea-transformed.glb" position={[0, -2, -3]} /> */}
+          <Gltf src="fiesta_tea-transformed.glb" position={[0, -2, -3]} />
         </Frame>
         <Frame id="03" name="still" author="Omar Faruq Tawsif" bg="#d1d1ca" position={[1.15, 0, 0]} rotation={[0, -0.5, 0]}>
-          {/* <Gltf src="still_life_based_on_heathers_artwork-transformed.glb" scale={2} position={[0, -0.8, -4]} /> */}
+          <Gltf src="still_life_based_on_heathers_artwork-transformed.glb" scale={2} position={[0, -0.8, -4]} />
         </Frame>
 
         <Rig />
