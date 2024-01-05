bd1174def9b074ef2ee6a8a7f8526a223e38b568
Jake Lee <firstvirtue@gmail.com>
2024-01-05 21:01:13 +0900

    Add design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,23 @@
 export function Footer() {
   return (
     <footer className="footer">
-      <div style={{minHeight: '20rem'}}></div>
+      <div className="footer-inner">
+        <div className="contact-wrap">
+          <a href="tel:+82.10.5260.0729">
+            <small>phone</small>
+            <span>+82.10.5260.0729</span>
+          </a>
+          <a href="mailto:firstvirtue@gmail.com">
+            <small>Pycsics</small>
+            <span>firstvirtue@gmail.com</span>
+          </a>
+        </div>
+        <div className="social-wrap">
+          <a href="#"><img className="social-icon" src='/assets/icons/linkedin-svgrepo-com.svg' /></a>
+          <a href="#"><img className="social-icon" src='/assets/icons/instagram-svgrepo-com.svg' /></a>
+          <a href="#"><img className="social-icon" src='/assets/icons/github-svgrepo-com.svg' /></a>
+        </div>
+      </div>
     </footer>
   )
 }
