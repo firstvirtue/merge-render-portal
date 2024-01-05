5c08122bf120e67855dd792d6f4c454a07070607
Jake Lee <firstvirtue@gmail.com>
2024-01-05 15:02:33 +0900

    Add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,19 @@
 export function Header() {
   return (
     <header className="header">
+      <div className="left">
+        <a href="/">
+          {/* <img src="/assets/img/j-logo2.png" alt="" /> */}
+          <strong>
+            Jake
+          </strong>
+          <strong>
+            Lee
+            <span className="dot">.</span>
+            <span>Contact</span>
+          </strong>
+        </a>
+      </div>
       <div className="menu-container">
         <a href="#" className="">Work</a>
         <a href="#" className="">Lab</a>
--- src/index.js
+++ src/index.js
@@ -38,9 +38,9 @@ function Root() {
         </a>
         <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>15/06/2023</div>
         <Logo style={{ position: 'absolute', bottom: 40, left: 40, width: 30 }} />
-        <a style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }} href="#" onClick={() => setLocation('/')}>
+        {/* <a style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }} href="#" onClick={() => setLocation('/')}>
           {params ? '< back' : 'double click to enter portal'}
-        </a>
+        </a> */}
       </div>{' '}
     </>
   )
